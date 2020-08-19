6f1283fc1de5d8092bf9cba334ad3ecb1d12d045
Gustavo <gustavo.motta23@gmail.com>
2020-08-19 20:19:46 -0300

    banco json OK

--- server/app/routes.js/medico/index.js
+++ server/app/routes.js/medico/index.js
@@ -1,4 +1,7 @@
 import { Router } from 'express'
+import * as data from '../../database/data.json'
+import fs from 'fs'
+
 
 const router = Router()
 
@@ -8,7 +11,25 @@ router.get('/', (req, res) => {
 
 router.post('/receita/nova', (req, res) => {
   console.log(req.body)
+  
   res.send('Receita ok!')
+})
+
+router.get('/json', (req, res) => {
+
+  data.pacientes.push({
+    name: 'walter',
+    id: data.pacientes.length + 1
+  })
+
+  let json = JSON.stringify(data)
+
+  fs.writeFile('app/database/data.json', json, (err) => {
+    if (err) return console.log(err)
+    console.log('sucesso!')
+  })
+
+  res.send(data.pacientes)
 })
 
 module.exports = router
