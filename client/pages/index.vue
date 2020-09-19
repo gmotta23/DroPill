d95b63787a64d9f93bce35daaa76c542b150d779
Gustavo <gustavo.motta23@gmail.com>
2020-09-19 09:41:26 -0300

    novo estilo - pagina de login

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="container">
-    <Header />
     <div class="page-content">
-      <h1 class="title-container">
-        DroPill
-      </h1>
-      <div class="login-container">
-        <div class="title">
-          Faça aqui seu login:
+      <div class="left-container">
+        <div class="dropill-small">
+          <img src="/img/logotipo.png" alt="">
         </div>
-        <form class="form">
-          <label for="email">
-            E-mail
-          </label>
-          <input id="email" type="email" v-model="form.email">
-          <label for="password">
-            Senha
-          </label>
-          <input id="password" type="password" v-model="form.senha">
-          <div class="button" @click.prevent="login(form)">
-            Entrar
-          </div>     
-        </form>
+        <div class="dropill-big">
+          <div class="logo-container">
+
+          </div>
+        </div>
+      </div>
+      <div class="right-container">
+        <div class="login-container">
+          <div class="titulo">
+            LOGIN
+          </div>
+          <div class="form">
+            <div class="input-container">
+              <input type="text" v-model="form.email">
+              <label for="email">E-mail</label>
+            </div>
+            <div class="input-container">
+              <input type="password" v-model="form.senha">
+              <label for="email">Senha</label>
+            </div>            
+          </div>
+          <button class="button" @click.prevent="login(form)">
+            ENTRAR
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -76,47 +84,100 @@ export default {
 
 .container {
   min-height: 100vh;
+  display: grid;
 }
 
 .page-content {
-  padding-top: 4rem;
-  display: flex;
-  align-items: center;
-  flex-flow: column;
-  background: url('/img/dp-tech.png');
-  background-repeat: no-repeat;
-  background-position: 50% 100%;
-  background-size: 50% 40%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  height: 100%;
+  background-color: #b8eee5;
 }
 
-.title-container {
-  width: 25vw;
-  margin-top: 2rem;
-  padding: 1rem;
-  // text-align: center;
-}
-
-.login-container {
-  width: 25vw;
-  height: 50vh;
-  display: flex;
-  flex-flow: column;
-  padding: 1rem;
-}
-
-.form {
-  display: flex;
-  flex-flow: column;
-  align-self: center;
-  margin-top: 1rem;
-  width: 100%;
-  input {
-    margin: 1rem 0;
+.left-container {
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  .dropill-small {
+    padding: 4rem;
+    img {
+      height: 10rem;
+    }
+  }
+  .dropill-big {
+    background-color: white;
+    border-radius: 2rem;
+    padding: 2rem;
+    margin: 4rem;
+    .logo-container {
+      background: url('/img/dp-tech.png');
+      background-repeat: no-repeat;
+      background-size: contain;
+      height: 100%;
+      width: 100%;
+    }
   }
 }
 
-.button {
-  @include mixin-button;
+.right-container {
+  padding: 4rem 2rem;
+  display: grid;
+  .login-container {
+    background-color: white;
+    border-radius: 2rem;
+    display: grid;
+    text-align: center;
+    padding: 4rem 2rem;
+    .titulo {
+      font-weight: bold;
+      font-size: 2rem;
+    }
+    .form {
+      // display: grid;
+      gap: .3rem;
+      padding: 0 2rem;
+      .input-container {
+        display: grid;
+        text-align: left;
+        margin: 1rem 0;
+        input {
+          height: 2rem;
+          margin: .5rem 0;
+          border-color: black;
+          border-width: 0 0 1px 0;
+          outline: 0;
+          &:focus {
+            border-color:  #317eaa;
+          }
+          // width: 15rem;
+        }
+        label {
+          font-weight: bold;
+          font-size: 1.2rem;
+        }
+      }
+    }
+    .button {
+      width: calc(100% - 4rem);
+      // margin: 0 2rem;
+      justify-self: center;
+      font-weight: bold;
+      font-size: 2rem;
+      border: 0;
+      outline: 0;
+      cursor: pointer;
+      border-radius: 1rem;
+      padding: 2rem;
+      background-color: #317eaa;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 2rem;
+      box-shadow: 2px 5px 10px #888888;
+      &:active {
+        transform: translateY(4px);
+      }
+    }
+  }
 }
 
 </style>
